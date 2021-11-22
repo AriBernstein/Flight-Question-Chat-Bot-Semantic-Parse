e22ec571cf0303b26f66ec98f9b643a4bd95a49c
Ari Bernstein <aribernstein1997@gmail.com>
2021-11-22 15:38:14 -0500

    realized issue, broke list str in trying to fix it

--- trips_parser_interface.py
+++ trips_parser_interface.py
@@ -17,7 +17,7 @@ PARSES_FIELD = "parses"
 TREE_FIELD = "tree"
 TERMS_FIELD = "terms"
 
-INDENT_LEN = 5
+INDENT_LEN = 3
 
 ITERABLE_CHECKS = {list, set, tuple}
 DICT_CHECKS = {dict, OrderedDict}
@@ -25,22 +25,18 @@ SKIP_FIELDS = {"rdf:RDF"}
 
 def call_trips_parser_api(sentence:str) -> dict:
     
-    resp = requests.get(
-        GET_URL + "?input=" + re.sub(r'\s+', '+', sentence) + \
-            "?output-parts=word-lisp lf-lisp")
+    resp = requests.get(GET_URL + "?input=" + re.sub(r'\s+', '+', sentence))
     if resp.status_code != 200:
         raise Exception(
-            f"Bad response when calling call_trips_parser_api\nCode: \
-                {resp.status_code}\nError: {resp.reason}\nURL: {resp.url}\t")
-    
+            "Bad response when calling call_trips_parser_api\nCode: " + \
+                f"{resp.status_code}\nError: {resp.reason}\nURL: {resp.url}\t")
     # Return dictionary    
     ret = {"sentence":sentence, PARSES_FIELD:[]}
     
     # Format XML output into new dictionary with input, parse tree 
     # representation, logical form representation
-    d = xmltodict.parse(resp.content)
-    d = d["trips-parser-output"]
-    
+    d = xmltodict.parse(resp.content)["trips-parser-output"]
+
     # Handle single or multiple parses
     if MULTIPLE_PARSES_FIELD in d:
         for parse in d[MULTIPLE_PARSES_FIELD][UTT_FIELD]:
@@ -54,41 +50,43 @@ def call_trips_parser_api(sentence:str) -> dict:
 
     return ret
 
-def dict_str(a:dict, indent:int=0, ret="", skip_fields:set[str]=None) -> str:
-    for k in a.keys():
+def dict_str(d:dict, indent:int=0, ret:str="", skip_fields:set[str]=None) -> str:
+    for k in d.keys():
         if skip_fields and k in skip_fields:
             continue
-        elif type(a[k]) in ITERABLE_CHECKS:
-            ret += list_str(l=a[k], indent=indent + INDENT_LEN, ret=ret)
-        # elif type(a[k]) in DICT_CHECKS:
-        #     ret += dict_str(a=a[k], indent=indent + INDENT_LEN, ret=ret)
+        elif type(d[k]) in ITERABLE_CHECKS:
+            ret += list_str(it=d[k], indent=indent + INDENT_LEN, ret=ret)
+        elif type(d[k]) in DICT_CHECKS:
+            ret += dict_str(d=d[k], indent=indent + INDENT_LEN, ret=ret)
         else:
-            ret += f"{ret}\n{' ' * indent}KEY: {k} -> {a[k]}"
+            ret += f"{ret}\n{' ' * indent}KEY: {k} -> {d[k]}"
     return ret
 
-def list_str(l:Iterable, indent:int=0, ret:str="", line_char_len:int=50) -> str:
-    if not l:
+def list_str(it:Iterable, indent:int=0, ret:str="", line_char_lim:int=50, skip_fields:set[str]=SKIP_FIELDS) -> str:
+    if not it:
         return "[empty list]"
     
-    list_str = f"{indent}["
-    for e in l:        
+    list_line = f"{' ' * indent}["
+    for e in it:        
         appended_to_ret = False
         if type(e) in ITERABLE_CHECKS:
-            list_str += list_str(e, indent + INDENT_LEN, ret, line_char_len)
+            print("list_str alalalala")
+            list_line += list_str(e, indent + INDENT_LEN, ret, line_char_lim, skip_fields)
         elif type(e) in DICT_CHECKS:
-            list_str += dict_str(e, indent + INDENT_LEN, ret)
+            print("list_str blblblblblblb")
+            list_line += dict_str(e, indent + INDENT_LEN, ret)
         else:
-            list_str += f"{e}, "
+            list_line += f"{e}, "
             
-        if len(list_str) >= line_char_len:
+        if len(list_line) >= line_char_lim:
             appended_to_ret = True
-            ret += f"\n{list_str}"
-            list_str = f" {indent}"
+            ret += f"\n{list_line}"
+            list_line = f" {' ' * indent}"
 
     if not appended_to_ret:
-        ret += f"\n{list_str}"
-    
-    return f"{ret[:-2]}]"
+        ret += f"\n {list_line}"
+
+    return ret
     
 if __name__ == "__main__":
     parser_data = call_trips_parser_api(SENTENCE)
@@ -101,5 +99,8 @@ if __name__ == "__main__":
     for i, v in enumerate(parser_data[PARSES_FIELD]):
         print(f"\n-> 2.{i} ----------\n")
         print(f"Interpretation {i}:")
-        print(f"\n\tParse Tree:\n\t{dict_str(v[0])}")
-        print(f"\n\tLogical Form:\n\t{dict_str(a=v[1], skip_fields=SKIP_FIELDS)}\n\n")
+        # print(f"\n\tParse Tree:\n\t{dict_str(v[0])}")
+        print(v)
+        print(list_str(v))
+
+        # print(f"\n\tLogical Form:\n\t{dict_str(a=v[1], skip_fields=SKIP_FIELDS)}\n\n")
